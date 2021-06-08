ff4efc4e3c55c12ce7229bf6adc60086bc3fd963
Juan Alvarez <juanmanuelag95@gmai.com>
2021-06-08 14:13:09 -0500

    Added the expected paths
    Todo: 
    - Read me file with the  instructions list
    - Test Matrix

--- Question_4/purgomalum/src/test/purgomalum.js
+++ Question_4/purgomalum/src/test/purgomalum.js
@@ -3,6 +3,7 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 const expect = require('chai').expect;
+const assert = require('chai').assert;
 
 chai.use(chaiHttp);
 const url= 'https://www.purgomalum.com/';
@@ -85,6 +86,123 @@ describe('PurgoMalum Automation API: ',()=>{
                     done();
                 });
         }); 
+
+        it('Does not contains profanity', (done) => {
+            chai.request(url)
+                .get('/service/containsprofanity')
+                .query({text: "this is some test input"})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    assert.equal(res.text, 'false')
+                    done();
+                });
+        }); 
+
+    });
+
+    context('Expected Paths', () => {
+        
+        it('Does contain profanity', (done) => {
+            chai.request(url)
+                .get('/service/containsprofanity')
+                .query({text: "ass"})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    assert.equal(res.text, 'true')
+                    done();
+                });
+        });
+        
+        it('Replacement text', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is some test input",
+                        add: "test",
+                        fill_text: "lorem"})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('result').to.be.equal("this is some lorem input");
+                    done();
+                });
+        });
+        
+        it('Replacement text multiple words', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is some test input",
+                        add: "test,input",
+                        fill_text: "lorem"})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('result').to.be.equal("this is some lorem  lorem");
+                    done();
+                });
+        });
+        
+        it('Replacement text case insesitive', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is some TEST input",
+                        add: "test",
+                        fill_text: "lorem"})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('result').to.be.equal("this is some lorem input");
+                    done();
+                });
+        });
+
+        it('Replacement text special chars', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is some test input",
+                        add: "test",
+                        fill_text: "_a_"})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('result').to.be.equal("this is some _a_ input");
+                    done();
+                });
+        });
+
+        it('Replacement text 20 chars lenght', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is some test input",
+                        add: "test",
+                        fill_text: "Lorem ipsum viverraa"})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('result').to.be.equal("this is some Lorem ipsum viverraa input");
+                    done();
+                });
+        });
+
+        it('Replacement char', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is some test input",
+                        add: "test",
+                        fill_char: "_"})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('result').to.be.equal("this is some ____ input");
+                    done();
+                });
+        });
+
+        it('Replacement char multiple words', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is some test input",
+                        add: "some , test",
+                        fill_char: "_"})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('result').to.be.equal("this is ____ ____ input");
+                    done();
+                });
+        });
 
     });
 });
