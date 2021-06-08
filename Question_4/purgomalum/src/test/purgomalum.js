a0d82ea4c84c865f4f37db6798b3a1bc86e5874c
Juan Alvarez <juanmanuelag95@gmai.com>
2021-06-07 21:06:15 -0500

    Boundaries/Edge Cases Cases Added

--- Question_4/purgomalum/src/test/purgomalum.js
+++ Question_4/purgomalum/src/test/purgomalum.js
@@ -11,16 +11,80 @@ const url= 'https://www.purgomalum.com/';
 describe('PurgoMalum Automation API: ',()=>{
 
     context('Boundaries Testing', () => {
-        it('should insert a country 1', (done) => {
+        it('Empty text value', (done) => {
             chai.request(url)
                 .get('/service/json')
-                .query({text: "this is some test input"})
+                .query({text: ""})
                 .end( function(err,res){
-                    console.log(res.body)
                     expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('error').to.be.equal("No Input");
                     done();
                 });
         });
+
+        it('Add parameter, bundaries more than 10 words', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is uno test input",
+                        add: "ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab",
+                        fill_text: "test "})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('error').to.be.equal("User Black List Exceeds Limit of 10 Words.");
+                    done();
+                });
+        });
+
+        it('Add parameter, bundaries more than 200 chars', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is uno test input",
+                        add: "Lorem ipsum dolor sit amet consectetur adipiscing elitNam velit erat blandit quis metus a eleifend commodo ligula sdsdSed vestibulum elementum neque eget variu Pelle ntesque sollicitudin sit morbi abcdefghi",
+                        fill_text: "test "})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('error').to.be.equal("Invalid Characters in User Black List");
+                    done();
+                });
+        });
+
+        it('Not profanity content on the text', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is some test input",
+                        fill_text: "test "})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('result').to.be.equal("this is some test input");
+                    done();
+                });
+        });
+        
+        it('Invalid replacement char', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is some test input",
+                        add: "test",
+                        fill_char: ";"})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('error').to.be.equal("Invalid User Replacement Characters");
+                    done();
+                });
+        }); 
+
+        it('Not present replacement text', (done) => {
+            chai.request(url)
+                .get('/service/json')
+                .query({text: "this is some test input",
+                        add: "test",
+                        fill_text: ""})
+                .end( function(err,res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('result').to.be.equal("this is some **** input");
+                    done();
+                });
+        }); 
 
     });
 });
